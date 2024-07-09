92ad611086d9627a8dd077305811e086d21f46e7
ahadsts9901 <ahadsts990@gmail.com>
2024-07-09 10:58:50 +0500

    updated-header

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -11,6 +11,7 @@ const Header = () => {
         <>
             <div className="header">
                 <p onClick={() => router.push("/jet")}>Jet</p>
+                <p onClick={() => router.push("/stone-valley")}>Stones</p>
             </div>
         </>
     )
